1fe6a5ed559ddb65ecd98342816773ecf0c1fd1e
Serdar Sayın <nefer.kha.ptah@gmail.com>
2019-10-10 16:43:39 +0300

    Integrate spark

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,27 @@
-public class App {
+import java.util.ArrayList;
 
+import static spark.Spark.port;
+
+public class App {
+    public boolean divisible(ArrayList<Integer> list, Integer div) {
+        for (Integer num : list) {
+            if (num % 2 > 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
