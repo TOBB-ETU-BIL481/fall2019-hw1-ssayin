60d6ef75eeca347102d03caf1a35db3332c47383
Serdar Sayın <nefer.kha.ptah@gmail.com>
2019-10-10 19:37:27 +0300

    Re-implement the static method, add tests

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,37 +1,89 @@
+import java.util.*;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
 
-import static spark.Spark.*;
+class App {
+    public static List<Row> matcher(ArrayList<String> firmName, String[] firmSymbols,
+                                      ArrayList<String> symbolIds, boolean matchCase) {
 
-public class App {
-    public boolean divisible(ArrayList<Integer> list, Integer div) {
-        for (Integer num : list) {
-            if (num % 2 > 0) {
-                return false;
+        Map<String, String> map = new HashMap<>();
+        /*
+         * We must make sure length of firmSymbols
+         * is not bigger than the length of symbolIds - IndexOutOfBoundsException
+         */
+        for (int i = 0; i < firmSymbols.length; ++i)
+            map.put(symbolIds.get(i), firmSymbols[i]);
+
+        List<Map.Entry<String, String>> list = new ArrayList<>(map.entrySet());
+        list.sort((o1, o2) -> {
+            if (o1.getValue().length() > o2.getValue().length()) return -1;
+            else if (o1.getValue().length() < o2.getValue().length()) return 1;
+            else return 0;
+        });
+
+        ArrayList<Row> ret = new ArrayList<>();
+        for (int i = 0; i < firmName.size(); ++i)
+            for (Map.Entry<String, String> obj : list) {
+                if (matchCase ? firmName.get(i).contains(obj.getValue()) :
+                        firmName.get(i).toLowerCase().contains(obj.getValue().toLowerCase())) {
+                    ret.add(new Row(obj.getKey(),
+                            firmName.get(i).replaceAll((matchCase ? "" : "(?i)") + obj.getValue(),
+                                    "[$0]")));
+                    break;
+                }
             }
-        }
+        return ret;
+    }
 
-        return true;
+
+    public static ArrayList<String> processInput(String input)
+    {
+        Scanner sc = new Scanner(input);
+        sc.useDelimiter("[;\r\n]+");
+
+        ArrayList<String> out = new ArrayList<>();
+        while (sc.hasNext()) out.add(sc.next());
+        return out;
     }
 
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
 
         post("/", (req, res) -> {
+            String input1 = req.queryParams("input1");
+            String input2 = req.queryParams("input2");
+            String input3 = req.queryParams("input3");
+            boolean input4 = (req.queryParams("input4") != null);
+
+            ArrayList<String> firmNames = processInput(input1);
+            ArrayList<String> firmIds = processInput(input2);
+            ArrayList<String> firmSymbols = processInput(input3);
+
+
+            List<Row> out = App.matcher(firmNames, firmSymbols.toArray(new String[0]),
+                    firmIds, input4);
+
+
             Map context = new HashMap<String, Object>();
+            context.put("matches", out);
 
-
-            return new ModelAndView(context, "app.mustache");
+            return new ModelAndView(context, "compute.mustache");
         }, new MustacheTemplateEngine());
+
 
         get("/",
                 (rq, rs) -> {
                     Map map = new HashMap();
-                    return new ModelAndView(map, "app.mustache");
+                    return new ModelAndView(map, "compute.mustache");
                 },
                 new MustacheTemplateEngine());
     }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,3 +1,114 @@
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AppTest {
+
+    private ArrayList<String> firms;
+    private ArrayList<String> syms;
+    private ArrayList<String> ids;
+
+    @BeforeAll
+    void initialize() {
+        firms = new ArrayList<>();
+        firms.add("Amazon");
+        firms.add("Microsoft");
+        firms.add("Google");
+        firms.add("TOBB ETU");
+
+        syms = new ArrayList<>();
+        syms.add("I");
+        syms.add("Am");
+        syms.add("cro");
+        syms.add("Na");
+        syms.add("le");
+        syms.add("abc");
+        syms.add("tobb");
+        syms.add("to");
+
+        ids = new ArrayList<>();
+        ids.add("101");
+        ids.add("117");
+        ids.add("26");
+        ids.add("68");
+        ids.add("193");
+        ids.add("33");
+        ids.add("6");
+        ids.add("90");
+    }
+
+    @Test
+    void matchCaseTest()
+    {
+        List<String> shouldBe = new ArrayList<>();
+        shouldBe.add("117[Am]azon");
+        shouldBe.add("26Mi[cro]soft");
+        shouldBe.add("193Goog[le]");
+
+        List<String> tmp = new ArrayList<>();
+
+        for (Row m : App.matcher(firms, syms.toArray(new String[0]), ids, true)) {
+            tmp.add(m.getId() + m.getName());
+        }
+
+        Assertions.assertArrayEquals(tmp.toArray(), shouldBe.toArray());
+    }
+
+    @Test
+    void matchCaseInsensitiveTest()
+    {
+        List<String> shouldBe = new ArrayList<>();
+        shouldBe.add("117[Am]azon");
+        shouldBe.add("26Mi[cro]soft");
+        shouldBe.add("193Goog[le]");
+        shouldBe.add("6[TOBB] ETU");
+
+        List<String> tmp = new ArrayList<>();
+
+        for (Row m : App.matcher(firms, syms.toArray(new String[0]), ids, false)) {
+            tmp.add(m.getId() + m.getName());
+        }
+
+        Assertions.assertArrayEquals(tmp.toArray(), shouldBe.toArray());
+    }
+
+    @Test
+    void inequalSymbolandIdCountTest()
+    {
+        ArrayList<String> temp = new ArrayList<>(syms);
+        temp.remove(0);
+        temp.remove(0);
+
+        List<String> shouldBe = new ArrayList<>();
+        shouldBe.add("117[Am]azon");
+        shouldBe.add("26Mi[cro]soft");
+        shouldBe.add("193Goog[le]");
+        shouldBe.add("6[TOBB] ETU");
+
+        List<String> tmp = new ArrayList<>();
+
+        for (Row m : App.matcher(firms, syms.toArray(new String[0]), ids, false)) {
+            tmp.add(m.getId() + m.getName());
+        }
+
+        Assertions.assertArrayEquals(tmp.toArray(), shouldBe.toArray());
+    }
+
+    @Test
+    void symsNullTest()
+    {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            App.matcher(firms, null, ids, false);
+        });
+    }
+
+    @Test
+    void symsEmptyListTest()
+    {
+        Assertions.assertArrayEquals(App.matcher(firms, new String[0], ids, false).toArray(), new String[0]);
+    }
 
 }
